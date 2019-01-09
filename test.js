444dff0121d53b88d0fcdbb39d51921435ed472a
immalaptop <immalaptop@immalappsMBP166.wifi.pcc.edu>
2019-01-09 14:37:51 -0800

    InitialUpload

---
+++ test.js
@@ -0,0 +1,10 @@
+function setup() {
+  createCanvas(800, 600);
+}
+
+function draw() {
+ // background(0);
+  ellipse(mouseX, mouseY, 80, 80);
+//  ellipse(100, 100, 100, 100);
+//  square(50, 50, 80, 80);
+}
